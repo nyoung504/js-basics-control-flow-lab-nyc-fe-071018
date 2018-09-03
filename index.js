c7986f832b29c5b09204b1b436aaf4d149a560a9
Nicole Young <coley504@gmail.com>
2018-09-03 02:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+ // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  const greeting = "This one is on me!";
+  if (distance <= 400) {
+    return 0;
+    else if
+  }
+}
